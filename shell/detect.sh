c8d4feb4933f365039f2f5dea3ece7af92d5be38
rystylee <rystylee8@gmail.com>
2019-06-27 13:24:46 +0900

    Added shell scripts to experiment

---
+++ shell/detect.sh
@@ -0,0 +1,4 @@
+python detect.py \
+cfg/yolo.cfg \
+weights/yolo.weights \
+test2.jpg
---
+++ shell/train.sh
@@ -0,0 +1,3 @@
+# python train_patch.py paper_obj
+# python train_patch.py exp4_class_only
+python train_patch.py exp1_des
